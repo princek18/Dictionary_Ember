14e625d37f6cb0f8ec23d3318eb875e52242a877
princek18 <princekumar181999@gmail.com>
2021-08-31 17:37:40 +0530

    modified using services

--- app/components/word.js
+++ app/components/word.js
@@ -1,36 +1,46 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking'
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class WordComponent extends Component {
-    @tracked word="";
-    @tracked word1;
-    @tracked result;
+    @service data;
+    @tracked word = '';
+    // @tracked word1;
+    //   @tracked result;
 
-    @action update(e){
+    @action update(e) {
         this.word = e.target.value;
     }
 
-    @action async search(e){
-        this.result = [];
-        let one = {};
+    @action search(e) {
         e.preventDefault();
-        let response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + this.word);
-        let result = await response.json();
-        this.word = "";
-        result.forEach(el => {
-            this.word1=el.word;
-            el.meanings.forEach(element => {
-                one.speech = element.partOfSpeech;
-                element.definitions.forEach(ele => {
-                    one.definition = ele.definition;
-                    one.example = ele.example;
-                });
-                this.result.push(one);
-                one = {};
-            });
-        });
-        console.log(this.word1);
-        console.log(this.result);
+        this.data.add(this.word);
+        this.word="";
     }
+
+    //   @action async search(e) {
+    //     this.result = [];
+    //     let one = {};
+    //     e.preventDefault();
+    //     let response = await fetch(
+    //       'https://api.dictionaryapi.dev/api/v2/entries/en/' + this.word
+    //     );
+    //     let result = await response.json();
+    //     this.word = '';
+    //     result.forEach((el) => {
+    //       this.word1 = el.word;
+    //       el.meanings.forEach((element) => {
+    //         one.speech = element.partOfSpeech;
+    //         element.definitions.forEach((ele) => {
+    //           one.definition = ele.definition;
+    //           one.example = ele.example;
+    //         });
+    //         this.result.push(one);
+    //         one = {};
+    //       });
+    //     });
+    //     console.log(this.word1);
+    //     console.log(this.result);
+    //   }
 }
